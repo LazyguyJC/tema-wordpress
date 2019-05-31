0e7c74774be6a2fc54c63aa6330df9f525588c95
LazyguyJC <jean.estevo78@gmail.com>
2019-05-31 09:34:56 -0400

    esse é o resultado no fim do curso, melhorias podem ser feitas

---
+++ search.php
@@ -0,0 +1,31 @@
+<?php get_header(  ); ?>
+    <div id="primary">
+        <div id="main">
+            <div class="container">
+
+                <h2>Search results for: </h2> <?php echo get_search_query(  ); ?>
+                <?php 
+
+                get_search_form();
+                
+                while( have_posts(  )): the_post(  );
+
+                    get_template_part('template-parts/content', 'search');
+
+                    if(comments_open() || get_comments_number()):
+                        comments_template();
+                    endif;
+                endwhile;
+
+                the_posts_pagination( 
+                    array(
+                        'prev-text' => 'Previous',
+                        'next-text' => 'next'
+                    )
+                 );
+
+                ?>
+            </div>
+        </div>
+    </div>
+<?php get_footer(  ); ?>
